ac09fd94de9e2688123fc99f47b8cc7440770148
aiyu666 <zrobin8741@gmail.com>
2020-05-09 14:27:51 +0800

    Remove no need request method

--- module/request.js
+++ module/request.js
@@ -3,18 +3,6 @@ const request = require('request');
 /**
  * @param  {object} options the request url, body ,headers
  */
-async function deleteRequest(options) {
-    return new Promise((resolve, reject) => {
-        request.delete(options, (error, response) => {
-            if (error) reject(new Error(`Ops! Some error about delete request -> ${error}`));
-            resolve(response);
-        });
-    });
-}
-
-/**
- * @param  {object} options the request url, body ,headers
- */
 async function getRequest(options) {
     return new Promise((resolve, reject) => {
         request.get(options, (error, response) => {
@@ -24,33 +12,6 @@ async function getRequest(options) {
     });
 }
 
-/**
- * @param  {object} options the request url, body ,headers
- */
-async function postRequest(options) {
-    return new Promise((resolve, reject) => {
-        request.post(options, (error, response) => {
-            if (error) reject(new Error(`Ops! Some error about post request-> ${error}`));
-            resolve(response);
-        });
-    });
-}
-
-/**
- * @param  {object} options the request url, body ,headers
- */
-async function putRequest(options) {
-    return new Promise((resolve, reject) => {
-        request.put(options, (error, response) => {
-            if (error) reject(new Error(`Ops! Some error about put request-> ${error}`));
-            resolve(response);
-        });
-    });
-}
-
 module.exports = {
-    deleteRequest,
     getRequest,
-    postRequest,
-    putRequest,
 };
